f8f3546a2f44187204d6e4d0d237d6d85502d911
ACH90 <chassagne.alexandre@gmail.com>
2025-04-22 15:00:06 +0200

    Dev Layout adding img & logo

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,18 @@
 // src/components/Layout.jsx
 import React from "react";
 import styles from "./Layout.module.css";
+import Logo from "/src/assets/SportSee_logo.png";
+import Nav from "/src/assets/nav-l.png";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layout}>
       <header className={styles.topHeader}>
-        <h1>Mon App Sportive</h1>
+        <img src={Logo} alt="" />
       </header>
       <div className={styles.mainContent}>
         <nav className={styles.sideNav}>
-          <ul>
-            <li>Accueil</li>
-            <li>Profil</li>
-            <li>Réglages</li>
-            <li>Communauté</li>
-          </ul>
+          <img src={Nav} alt="" />
         </nav>
         <main className={styles.pageContent}>{children}</main>
       </div>
