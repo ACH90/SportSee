27506d90bfd060c314e8448a77ef14dc30423fe1
ACH90 <chassagne.alexandre@gmail.com>
2025-04-23 16:15:32 +0200

    Add components

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,9 @@ import { createBrowserRouter } from "react-router-dom";
 import UserProfile from "../components/UserProfile/UserProfile.jsx";
 // Layout
 import Layout from "../Layout/Layout.jsx";
+import Activity from "../components/Activity/Activity.jsx";
+import AverageSession from "../components/AverageSession/AverageSession.jsx";
+import Perf from "../components/Perf/Perf.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,18 @@ const router = createBrowserRouter([
       {
         path: "user/:userId", // Relatif, sans "/"
         element: <UserProfile />,
+      },
+      {
+        path: "user/:userId/activity",
+        element: <Activity />,
+      },
+      {
+        path: "user/:userId/average-sessions",
+        element: <AverageSession />,
+      },
+      {
+        path: "user/:userId/performance",
+        element: <Perf />,
       },
     ],
   },
