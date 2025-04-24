392bce7a63b04cbc7ddf239e8fb817308785184e
ACH90 <chassagne.alexandre@gmail.com>
2025-04-24 17:10:19 +0200

    Add RadarChart Perf

--- src/components/Perf/Perf.jsx
+++ src/components/Perf/Perf.jsx
@@ -1,5 +1,13 @@
 import { useParams } from "react-router-dom";
 import useUserPerf from "../../utils/useUserPerf";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from "recharts";
 
 const Perf = () => {
   const { userId } = useParams();
@@ -11,17 +19,36 @@ const Perf = () => {
   if (!userPerf || !userPerf.data || !userPerf.kind) {
     return <div>Aucune donnée de performance disponible.</div>;
   }
-
+  console.log(userPerf.kind);
+  console.log(userPerf.data);
+  const formattedData = userPerf.data.map((item) => ({
+    subject: userPerf.kind[item.kind],
+    value: item.value,
+  }));
   return (
     <div>
       <h2>Performances</h2>
       <ul>
-        {userPerf.data.map((perf, index) => (
+        {formattedData.map((perf, index) => (
           <li key={index}>
-            {userPerf.kind[perf.kind]} : {perf.value}
+            {perf.subject} : {perf.value}
           </li>
         ))}
       </ul>
+      <ResponsiveContainer width="100%" height={300}>
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={formattedData}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar
+            name="Perf"
+            dataKey="value"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
